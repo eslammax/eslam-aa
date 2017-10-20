1a31408f0bd44e0ef97c4d65626cba5e0de11341
eslammax <eslamfox233@gmail.com>
2017-10-20 21:28:18 +0200

    Update and rename eslam.php to esla31m.php

--- esla31m.php
+++ esla31m.php
@@ -1,11 +1,7 @@
-            </div><!--End Quality-list-->
-        </div><!--End Channel-quality-->
-        <div class="channel-vedio" id="video_container">
+ <div class="channel-vedio" id="video_container">
             <script type='text/javascript'>
 var width= document.getElementById("video_container").offsetWidth, 
  height= getVideoHeight() ,
  channel='ASDSDFGWQERSDF', e='1';
 </script>
 <script type='text/javascript' src='http://www.mipsplayer.net/content/scripts/hmipsEmbed.js'></script>
-        </div><!--End Channel-vedio-->
-    </div><!--End Page-Content-->
